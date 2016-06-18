13386320b33388936fc24e284c0c033e17df44d6
Alex Sink <alexsink@yahoo.com>
2016-06-18 13:15:15 -0700

    new: initial setup of spawning from a raycast that should work wth spacial mapping

---
+++ mainproject/Assets/buildingBlocks/PlaceRobotInSpace/SelectHardObjectAndSpawnRelative.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class SelectHardObjectAndSpawnRelative : MonoBehaviour
+{
+	[SerializeField] private GameObject _prefabToSpawn;
+	private GameObject _spawnedPrefab;
+	[SerializeField] private readonly Vector3 offsetFromTarget = new Vector3(.5f, 0f, 0f);
+
+	private GestureRecognizer recognizer;
+	public GameObject FocusedObject { get {return _spawnedPrefab;} }
+
+	// Use this for initialization
+	private void Start()
+	{
+		recognizer = new GestureRecognizer();
+		recognizer.TappedEvent += RecognizerOnTappedEvent;
+		recognizer.StartCapturingGestures();
+		spawn(new Vector3(0, 0.91f, -9.59f));
+	}
+
+	private void RecognizerOnTappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+	{
+		var hit = new RaycastHit();
+		//if (Physics.Raycast(headPosition, gazeDirection, out hitInfo,
+		//30.0f, SpatialMapping.PhysicsRaycastMask))
+		//layerMask:SpatialMapping.PhysicsRaycastMask
+		if(Physics.Raycast(headRay, out hit))
+			spawn(hit.transform.position + offsetFromTarget);
+	}
+
+	private void spawn(Vector3 positionToSpawnAt)
+	{
+		var spawned = _spawnedPrefab ?? Instantiate(_prefabToSpawn);
+		spawned.transform.position = positionToSpawnAt;
+		spawned.transform.LookAt(Camera.main.transform.position);
+		_spawnedPrefab = spawned;
+	}
+}
