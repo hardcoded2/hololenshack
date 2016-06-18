a8e9909b0550de0729c143f0c708ce1686b12ed3
Alex Sink <alexsink@yahoo.com>
2016-06-18 15:45:39 -0700

    Sign the letters of what the person is saying scene

--- mainproject/Assets/buildingBlocks/dictationManagerTest/SignWhatIsDictated.cs
+++ mainproject/Assets/buildingBlocks/dictationManagerTest/SignWhatIsDictated.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using UnityEngine;
-using System.Collections;
 using UnityEngine.Windows.Speech;
-using Object = UnityEngine.Object;
 
-public class SignWhatIsDictated : MonoBehaviour {
-	private int samplingRate;
+public class SignWhatIsDictated : MonoBehaviour
+{
+	private const int messageLength = 60;
 	// Using an empty string specifies the default microphone. 
-	private static string deviceName = string.Empty;
+	private static readonly string deviceName = string.Empty;
 
 	private DictationRecognizer dictationRecognizer;
-	private const int messageLength = 10;
+	private int samplingRate;
 
 	// Use this for initialization
-	void Start () {
-		dictationRecognizer = new DictationRecognizer(ConfidenceLevel.High,DictationTopicConstraint.Dictation);
+	private void Start()
+	{
+		dictationRecognizer = new DictationRecognizer(ConfidenceLevel.High, DictationTopicConstraint.Dictation);
 		dictationRecognizer.Start();
 		dictationRecognizer.DictationResult += DictationRecognizerOnDictationResult;
 
@@ -22,43 +22,52 @@ public class SignWhatIsDictated : MonoBehaviour {
 		// Query the maximum frequency of the default microphone. Use 'unused' to ignore the minimum frequency.
 		int unused;
 		Microphone.GetDeviceCaps(deviceName, out unused, out samplingRate);
+		//StartRecording();
 	}
 
 	private void DictationRecognizerOnDictationResult(string text, ConfidenceLevel confidence)
 	{
+		Debug.Log("Got dictation result:" + text);
 		try
 		{
-			foreach(var speechManager in Object.FindObjectsOfType<BlendLegacyAnimations>())
+			foreach (var speechManager in FindObjectsOfType<BlendLegacyAnimations>())
 			{
 				speechManager.SayWordFromVoiceRecgonition(text);
 			}
-		}catch(Exception e) { Debug.LogError("Error when oding speech:"+e.Message);}
-		
+		}
+		catch (Exception e)
+		{
+			Debug.LogError("Error when oding speech:" + e.Message);
+		}
 	}
+
 	/// <summary>
-	/// Turns on the dictation recognizer and begins recording audio from the default microphone.
+	///     Turns on the dictation recognizer and begins recording audio from the default microphone.
 	/// </summary>
 	/// <returns>The audio clip recorded from the microphone.</returns>
 	public AudioClip StartRecording()
 	{
+		Debug.Log("Startrecording");
 		// Shutdown the PhraseRecognitionSystem. This controls the KeywordRecognizers
 		PhraseRecognitionSystem.Shutdown();
 
 		// Start dictationRecognizer
 		dictationRecognizer.Start();
 
-		Debug.Log( "Dictation is starting. It may take time to display your text the first time, but begin speaking now...");
+		Debug.Log("Dictation is starting. It may take time to display your text the first time, but begin speaking now...");
 
 		// Start recording from the microphone for 10 seconds
-		return Microphone.Start(deviceName, false, messageLength, samplingRate);
+		var clip = Microphone.Start(deviceName, false, messageLength, samplingRate);
+		Debug.Log("GOT Audio clip");
+		return clip;
+		;
 	}
+
 	public void StopRecording()
 	{
 		// Check if dictationRecognizer.Status is Running and stop it if so
 		if(dictationRecognizer.Status == SpeechSystemStatus.Running)
-		{
 			dictationRecognizer.Stop();
-		}
 
 		Microphone.End(deviceName);
 	}
