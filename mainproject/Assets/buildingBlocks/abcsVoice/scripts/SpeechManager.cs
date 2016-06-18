05daa6aa18a0818ef75525ec3166d04ebe1ae2d7
Priyank Jain <fly2priyank@gmail.com>
2016-06-18 13:41:39 -0700

    Revert "Revert "speech recognition and triggering signs""
    This reverts commit 7cb211431c6a170694d0d0e21406da74ee48ef91.

---
+++ mainproject/Assets/buildingBlocks/abcsVoice/scripts/SpeechManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour
+{
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // reference to the animation manager
+    BlendLegacyAnimations animationMgr;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.animationMgr = GetComponent<BlendLegacyAnimations>();
+
+        keywords.Add(this.animationMgr.textToSay, () =>
+        {
+            this.animationMgr.SayWordFromVoiceRecgonition(this.animationMgr.textToSay);
+        });
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
