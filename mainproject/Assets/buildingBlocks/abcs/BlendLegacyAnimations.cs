7cb211431c6a170694d0d0e21406da74ee48ef91
Priyank Jain <fly2priyank@gmail.com>
2016-06-18 13:38:54 -0700

    Revert "speech recognition and triggering signs"
    This reverts commit 7877043627d0bb933dec31be8f61f4dc6049ed0e.

--- mainproject/Assets/buildingBlocks/abcs/BlendLegacyAnimations.cs
+++ mainproject/Assets/buildingBlocks/abcs/BlendLegacyAnimations.cs
@@ -18,11 +18,11 @@ public class BlendLegacyAnimations : MonoBehaviour
 	{
 		_animation = GetComponent<Animation>();
 		youGotTold.Init();
-		//StartCoroutine(youGotTold.SayWord(textToSay, _animation));
+		StartCoroutine(youGotTold.SayWord(textToSay, _animation));
 
 	}
 
-	public void SayWordFromVoiceRecgonition(string word)
+	void SayWordFromVoiceRecgonition(string word)
 	{
 		StartCoroutine(youGotTold.SayWord(word, _animation));
 	}
